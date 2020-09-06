7b958fd1d996814e0664bbb241ff01a9b297dc1d
herocatlala <1132188964@qq.com>
2020-09-06 21:20:04 +0800

    更新

---
+++ JAVA/basic_level/Main_1071.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+/**
+ * by:os0day
+ * title:PAT,乙等,1071 小赌怡情 (15分)
+ * date:2020-09-02
+ */
+public class Main_1071 {
+    static Scanner sc = new Scanner(System.in);
+    static int sumT,numK;
+    public static void main(String[] args) {
+        while (sc.hasNext()){
+
+            sumT = sc.nextInt();
+            numK = sc.nextInt();
+            int flag = 0;
+            for (int i = 0; i < numK; i++){
+                if (0<sumT){
+                    Output(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+                }else {
+                    System.out.println("Game Over.");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag==1)break;
+        }
+    }
+
+    static void  Output(int n1,int b,int t,int n2){
+       int flag;
+       if(n1<n2){
+           flag = 1;
+       }else {
+           flag = 0;
+       }
+       if(t<=sumT&&sumT>=0) {
+           if (flag == b) {
+               sumT += t;
+               System.out.println("Win " + t + "!  Total = " + sumT + ".");
+           } else {
+               sumT -= t;
+               System.out.println("Lose " + t + "." + "  Total = " + sumT + ".");
+           }
+       }else {
+           System.out.println("Not enough tokens.  Total = "+sumT+".");
+       }
+    }
+}
+/*
+1071 小赌怡情 (15分)
+常言道“小赌怡情”。这是一个很简单的小游戏：首先由计算机给出第一个整数；然后玩家下注赌第二个整数将会比第一个数大还是小；玩家下注 t 个筹码后，计算机给出第二个数。若玩家猜对了，则系统奖励玩家 t 个筹码；否则扣除玩家 t 个筹码。
+
+注意：玩家下注的筹码数不能超过自己帐户上拥有的筹码数。当玩家输光了全部筹码后，游戏就结束。
+
+输入格式：
+输入在第一行给出 2 个正整数 T 和 K（≤ 100），分别是系统在初始状态下赠送给玩家的筹码数、以及需要处理的游戏次数。随后 K 行，每行对应一次游戏，顺序给出 4 个数字：
+
+n1 b t n2
+其中 n1 和 n2 是计算机先后给出的两个[0, 9]内的整数，保证两个数字不相等。b 为 0 表示玩家赌小，为 1 表示玩家赌大。t 表示玩家下注的筹码数，保证在整型范围内。
+
+输出格式：
+对每一次游戏，根据下列情况对应输出（其中 t 是玩家下注量，x 是玩家当前持有的筹码量）：
+
+玩家赢，输出 Win t!  Total = x.；
+玩家输，输出 Lose t.  Total = x.；
+玩家下注超过持有的筹码量，输出 Not enough tokens.  Total = x.；
+玩家输光后，输出 Game Over. 并结束程序。
+输入样例 1：
+100 4
+8 0 100 2
+3 1 50 1
+5 1 200 6
+7 0 200 8
+输出样例 1：
+Win 100!  Total = 200.
+Lose 50.  Total = 150.
+Not enough tokens.  Total = 150.
+Not enough tokens.  Total = 150.
+输入样例 2：
+100 4
+8 0 100 2
+3 1 200 1
+5 1 200 6
+7 0 200 8
+输出样例 2：
+Win 100!  Total = 200.
+Lose 200.  Total = 0.
+Game Over.
+ */
